373d1457243f130b2e21e5fafe5374968f346806
Mahmoud Nasr <239.nasr@gmail.com>
2025-01-20 21:24:16 +0200

    Table

---
+++ app/components/TableGrid.tsx
@@ -0,0 +1,11 @@
+import TableItem from "./TableItem";
+
+const TableGrid = () => {
+    return <div className='flex flex-col gap-3 items-center justify-center'>
+            
+           <TableItem />
+           <TableItem/>
+        </div>
+};
+
+export default TableGrid;
---
+++ app/components/TableItem.tsx
@@ -0,0 +1,15 @@
+const TableItem = ({ item }) => {
+
+    return (
+        <div className="flex flex-row gap-3 ">
+        <div>
+            <h2 className="text-2xl font-bold">09:00 AM</h2>
+        </div>
+        <div>
+            <h2 className="text-2xl font-bold">Mahmoud Auf</h2>
+        </div>
+    </div>
+    );
+}
+
+export default TableItem;
