9c2db40ee1608036b6f74e921742f2ae04d26a99
Mahmoud Nasr <239.nasr@gmail.com>
2025-01-20 21:52:37 +0200

    Some UI Changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import moment from "moment";
 
 export default function Home() {
 	return (
-		<div className='flex flex-col gap-8 min-h-screen p-8'>
+		<div className='flex flex-col gap-8 min-h-screen p-8 '>
 			<main className='flex flex-col gap-3  m-auto'>
 				<div className='flex flex-col gap-3 items-center justify-center'>
 					<Image
@@ -15,21 +15,22 @@ export default function Home() {
 						className='rounded-lg w-12'
 						priority
 					/>
-					<h1 className='text-xl font-bold bg-gradient-to-br from-fuchsia-500 to-rose-500 text-transparent bg-clip-text'>
+					<h1 className='text-xl font-bold bg-gradient-to-br from-fuchsia-500 to-rose-500 text-transparent bg-clip-text text-center'>
 						We are ITI Alexandria PWD Intake 45
 					</h1>
-
 				</div>
 
 				<div className=' text-center'>
-					<h2 className='text-2xl  font-bold text-emerald-600 '>Our Schedule For Today ({moment().format("MMM DD")})</h2>
+					<h2 className='text-2xl  font-bold text-emerald-600 '>
+						Our Schedule For Today ({moment().format("MMM DD")})
+					</h2>
 				</div>
-        <TableGrid />
-
+				<TableGrid />
 			</main>
-			<footer className='flex flex-wrap items-center justify-center  antialiased text-sm '>
-				Made with ❤️ @ IT Alexandria Labs by{" "}
+			<footer className='text-center text-sm font-black font-mono'>
+				<span>Made with ❤️ @ IT Alexandria Labs by</span>
 				<a
+					className=' decoration-sky-500 underline decoration-2	 ml-1'
 					href='https://github/gitnasr'
 					referrerPolicy='no-referrer'
 					target='_blank'>
