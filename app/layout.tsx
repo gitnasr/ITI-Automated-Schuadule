22355ba93abd14c578bc6f23e2878c14024e7e94
Mahmoud Nasr <239.nasr@gmail.com>
2025-01-21 12:39:34 +0200

    Make the Footer Globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "ITI Intake 45 Schedule",
   description: "An Automated Service for ITI Intake 45 Schedule",
+  applicationName:"mySchedule",
+  manifest: "/manifest.ts",
+  
+ 
 };
 
 export default function RootLayout({
@@ -30,6 +34,23 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <footer className='text-center text-sm font-black font-mono fixed bottom-5 left-0 right-0'>
+				<span>Made with ❤️ @ ITI Alexandria Labs by</span>
+				<a
+					className=' decoration-sky-500 underline decoration-2	 ml-1'
+					href='https://github.com/gitnasr'
+					referrerPolicy='no-referrer'
+					target='_blank'>
+					NASR
+				</a>
+
+        <span className='mx-1'>| <a className="underline decoration-white "
+        referrerPolicy='no-referrer'
+					target='_blank'
+          href="https://docs.google.com/spreadsheets/d/15niRN3yDfeglOy4UPiYB7UrpOxhp83slN4BJcD2HAvs"
+        
+        >Sheet URL</a></span>
+			</footer>
       </body>
     </html>
   );
