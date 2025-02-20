491cc3754982ab566dc3293db1e4d2d31bc707e6
Mahmoud Nasr <239.nasr@gmail.com>
2025-02-20 14:34:02 +0200

    Added Cross Origin Confiuration to the new domain

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,24 @@ import withPWAInit from "@ducanh2912/next-pwa";
 const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
+  async headers() {
+    return [
+        {
+            source: "/api/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "45.gitnasr.com" },
+                { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                { key: "Access-Control-Allow-Headers", value: "*" },
+            ]
+        }
+    ]
+}
 });
 
 export default withPWA({
   publicRuntimeConfig: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    
 }
 });
